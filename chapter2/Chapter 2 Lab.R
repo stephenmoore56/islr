44f3a453cc42bceae1a84f57e79ff40d131c1846
Stephen Moore <stephenmoore56@msn.com>
2015-12-07 11:11:29 -0600

    Chapter 2 lab script

---
+++ chapter2/Chapter 2 Lab.R
@@ -0,0 +1,167 @@
+# Chapter 2 Lab: Introduction to R
+
+# Basic Commands
+
+x <- c(1,3,2,5)
+x
+x = c(1,6,2)
+x
+y = c(1,4,3)
+length(x)
+length(y)
+# element-wise operations; matrices must be
+# same size in most cases
+x+y
+x/y
+x^y
+
+# accessing elements in a vector
+x[2]
+x[2:3]
+x[-2]
+
+# matrices / 2D arrays
+# slicing and dicing
+?matrix
+
+x=matrix(data=c(1,2,3,4), nrow=2, ncol=2)
+x
+x=matrix(c(1,2,3,4),2,2)
+matrix(c(1,2,3,4),2,2,byrow=TRUE)
+
+z = matrix(seq(1,12),4,3)
+z
+z[3:4,2:3]
+z[,1]
+z[,1,drop=F]
+
+# listing and removing variables in memory
+ls()
+rm(x,y)
+ls()
+rm(list=ls())
+
+# generating data
+x = runif(50)
+x
+hist(x)
+
+y = rnorm(50)
+hist(y)
+
+plot(x,y)
+plot(x,y,
+     xlab="Random Uniform",ylab="Random Normal",
+     pch=21,col="blue")
+
+par(mfrow=c(3,1))
+plot(x,y)
+plot(x,y,
+     xlab="Random Uniform",ylab="Random Normal",
+     pch=21,col="blue")
+hist(y)
+par(mfrow=c(1,1))
+
+# reading in data
+Auto = read.csv("Auto.csv")
+names(Auto)
+str(Auto)
+dim(Auto)
+class(Auto)
+summary(Auto)
+plot(Auto$cylinders,Auto$mpg)
+attach(Auto) # create a workspace for Auto data
+search()
+
+plot(cylinders,mpg)
+
+sqrt(x)
+x^2
+x=rnorm(50)
+y=x+rnorm(50,mean=50,sd=.1)
+cor(x,y)
+set.seed(1303)
+rnorm(50)
+set.seed(3)
+y=rnorm(100)
+mean(y)
+var(y)
+sqrt(var(y))
+sd(y)
+
+# Graphics
+
+x=rnorm(100)
+y=rnorm(100)
+plot(x,y)
+plot(x,y,xlab="this is the x-axis",ylab="this is the y-axis",main="Plot of X vs Y")
+pdf("Figure.pdf")
+plot(x,y,col="green")
+dev.off()
+x=seq(1,10)
+x
+x=1:10
+x
+x=seq(-pi,pi,length=50)
+y=x
+f=outer(x,y,function(x,y)cos(y)/(1+x^2))
+contour(x,y,f)
+contour(x,y,f,nlevels=45,add=T)
+fa=(f-t(f))/2
+contour(x,y,fa,nlevels=15)
+image(x,y,fa)
+persp(x,y,fa)
+persp(x,y,fa,theta=30)
+persp(x,y,fa,theta=30,phi=20)
+persp(x,y,fa,theta=30,phi=70)
+persp(x,y,fa,theta=30,phi=40)
+
+# Indexing Data
+
+A=matrix(1:16,4,4)
+A
+A[2,3]
+A[c(1,3),c(2,4)]
+A[1:3,2:4]
+A[1:2,]
+A[,1:2]
+A[1,]
+A[-c(1,3),]
+A[-c(1,3),-c(1,3,4)]
+dim(A)
+
+# Loading Data
+
+Auto=read.table("Auto.data")
+fix(Auto)
+Auto=read.table("Auto.data",header=T,na.strings="?")
+fix(Auto)
+Auto=read.csv("Auto.csv",header=T,na.strings="?")
+fix(Auto)
+dim(Auto)
+Auto[1:4,]
+Auto=na.omit(Auto)
+dim(Auto)
+names(Auto)
+
+# Additional Graphical and Numerical Summaries
+
+plot(cylinders, mpg)
+plot(Auto$cylinders, Auto$mpg)
+attach(Auto)
+plot(cylinders, mpg)
+cylinders=as.factor(cylinders)
+plot(cylinders, mpg)
+plot(cylinders, mpg, col="red")
+plot(cylinders, mpg, col="red", varwidth=T)
+plot(cylinders, mpg, col="red", varwidth=T,horizontal=T)
+plot(cylinders, mpg, col="red", varwidth=T, xlab="cylinders", ylab="MPG")
+hist(mpg)
+hist(mpg,col=2)
+hist(mpg,col=2,breaks=15)
+pairs(Auto)
+pairs(~ mpg + displacement + horsepower + weight + acceleration, Auto)
+plot(horsepower,mpg)
+identify(horsepower,mpg,name)
+summary(Auto)
+summary(mpg)
