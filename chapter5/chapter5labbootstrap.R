dcdb9398ec13e757fe6f6289601e5e36b9230f5e
Stephen Moore <stephenmoore56@msn.com>
2015-12-28 11:48:54 -0600

    Forgot to save script

--- chapter5/chapter5labbootstrap.R
+++ chapter5/chapter5labbootstrap.R
@@ -5,9 +5,21 @@ alpha = function(x,y) {
   vx = var(x)
   vy = var(y)
   cxy = cov(x,y)
-  # (vy - cxy)/(vx + vy - 2*cxy)
+  (vy - cxy)/(vx + vy - 2*cxy)
 }
 
 alpha(Portfolio$X, Portfolio$Y)
-Portfolio
-class(Portfolio$X)
+
+alpha.fn = function(data, index) {
+  with(data[index,],alpha(X,Y))
+}
+alpha.fn(Portfolio,1:100)
+
+
+# run a bootstrap
+set.seed(1)
+alpha.fn(Portfolio,sample(1:100,100,replace=TRUE))
+
+boot.out = boot(Portfolio,alpha.fn,R=1000)
+plot(boot.out)
+boot.out
